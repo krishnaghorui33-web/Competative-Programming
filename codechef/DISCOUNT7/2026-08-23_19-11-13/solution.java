44c22a1005ef5bceccffda40bf214ef47d7163aa
krishnaghorui33 <krishnaghorui33@gmail.com>
2026-08-23 19:11:16 +0530

    Add codechef/DISCOUNT7 solution at 2026-08-23_19-11-13

---
+++ codechef/DISCOUNT7/2026-08-23_19-11-13/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here  
+		Scanner sc = new Scanner(System.in); 
+		int X = sc.nextInt(); 
+		if(X >= 5){
+		    System.out.println( X * 85);
+		}else{
+		    System.out.println(X * 100);
+		}
+
+	}
+}
