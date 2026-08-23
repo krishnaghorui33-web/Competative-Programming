4dfcbfeffa9d6aa7279751746890f9d4009c39be
krishnaghorui33 <krishnaghorui33@gmail.com>
2026-08-23 19:12:47 +0530

    Add codechef/SQPAPER solution at 2026-08-23_19-12-44

---
+++ codechef/SQPAPER/2026-08-23_19-12-44/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int side=Math.min(a,b);
+		System.out.println(side*side);
+
+	}
+}
