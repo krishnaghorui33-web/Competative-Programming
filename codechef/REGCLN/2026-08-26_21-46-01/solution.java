7af312694d446eebd7f60963cefdfd2c94f96889
krishnaghorui33 <krishnaghorui33@gmail.com>
2026-08-26 21:46:02 +0530

    Add codechef/REGCLN solution at 2026-08-26_21-46-01

---
+++ codechef/REGCLN/2026-08-26_21-46-01/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		int d=t%10;
+		d=10-d;
+		System.out.println(d);
+
+	}
+}
